ec4a917aae3ed2eb13290dc3d4ef01f1705abcab
Sri Astie <141595421+sriastutie@users.noreply.github.com>
2023-08-21 08:40:56 +0700

    Create dataguru.php
    gordes

---
+++ dataguru.php
@@ -0,0 +1,11 @@
+di sini adalah halaman untuk menampilkan data guru dengan menggunakan table
+data guru terdiri dari
+id
+nama
+jenis kelamin
+Alamat
+Jenis Kelamin
+mata pelajaran
+Action:
+ -Edit
+ -Delete
